ee9b03a876f910e4559dc823c4159f9902a8c5d3
Max Carter <undivisible@vk.com>
2025-05-17 18:42:46 +1000

    hotfix 0.2.0-alpha

--- functions/inject-env.js
+++ functions/inject-env.js
@@ -1,8 +1,8 @@
 // This file injects environment variables into the client-side code for Netlify deployment
 
-export const handler = async function(event, context) {
+exports.handler = async function(event, context) {
   // Get the HTML content from the SvelteKit render function
-  const { handler } = await import('../.netlify/functions-internal/sveltekit-render.mjs');
+  const { handler } = require('../.netlify/functions-internal/sveltekit-render.mjs');
   const response = await handler(event, context);
   
   // Only process HTML responses
